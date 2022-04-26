eecfeee7587f25ead4bf048088a012e8b7f59db6
Alexandra Davis <alex.davis806@gmail.com>
2022-04-26 08:51:11 -0700

    commit tree b5a668e99073a7e48d2092da6961ea08c109a592

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,28 @@
+require 'pry'
+
 class Dog
 
+    def name=(name)
+        @name = name
+    end
+
+    def name
+        @name
+    end
+
+    def breed=(breed)
+        @breed = breed
+    end
+
+    def breed
+        @breed 
+    end
 end
+
+rebel = Dog.new
+ 
+rebel.name = 'rebel'
+rebel.name
+
+binding.pry
+
